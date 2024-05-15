4b512d2143e5e7bfe01b0b71258fb630f8b4970c
ghulam mohi ud din <GHULAM-1>
2024-05-16 03:35:38 +0500

    removed grid functions

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/marketing/hero";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <Hero></Hero>
 
-      <UserButton></UserButton>
     </>
   );
 }
--- src/lib/db-connect.ts
+++ src/lib/db-connect.ts
@@ -1,30 +1,15 @@
 import mongoose from "mongoose";
-import fs from "fs";
 
-const connectDB = async () => {
+const connectdb = async () => {
   try {
-    if (mongoose.connection.readyState === 0) {
-      await mongoose.connect(
-        "mongodb+srv://ghulam:1234@cluster0.jsqqnid.mongodb.net/vr-db?retryWrites=true&w=majority&appName=Cluster0"
-      );
-      console.log("db connected");
+    await mongoose.connect(
+      "mongodb+srv://ghulam:1234@cluster0.17n9uau.mongodb.net/cc-db?retryWrites=true&w=majority&appName=Cluster0"
+    );
 
-      const db = mongoose.connection.db; // Accessing the underlying native MongoDB driver
-
-      const { GridFSBucket } = mongoose.mongo; // Access GridFSBucket through mongoose.mongo
-
-      const bucket = new GridFSBucket(db, { bucketName: "fs" }); // Create GridFSBucket instance
-      console.log("GridFSBucket created");
-      fs.createReadStream("./1.jpg").pipe(
-        bucket.openUploadStream("myFile", {
-          chunkSizeBytes: 1048576,
-          metadata: { field: "myField", value: "myValue" },
-        })
-      );
-    }
+    console.log("database connected 🔥🔥🔥");
   } catch (error) {
     console.log(error);
   }
 };
 
-export default connectDB;
+export default connectdb;
